1b037355e550b0bb9661e865ef241d239c275b2c
Erik200031 <ero.stepanyan.00@gmail.com>
2022-06-02 21:51:14 +0400

    variant

--- variant/test.cpp
+++ variant/test.cpp
@@ -19,30 +19,46 @@
 //     return t.get_pack();
 // }
 
-template < typename Variant, size_t i>
-int get_pack_(Variant t) {
-    return 111111;
-}
 
-// template <typename Variant>
-// auto get_pack_<Variant, 0>(Variant& t) {
-//     return t;
-// }
+template <typename Variant, size_t IDX>
+struct get_pack_
+{
+    get_pack_(const Variant& t) : ob{t} {}
+    auto operator()() {
+        return get_pack_<decltype(ob.get_pack()), IDX - 1>(ob.get_pack());
+    }
+    Variant ob;
+};
+
 
 template <typename Variant>
- void get_pack_<0>(Variant t) {
-     std::cout << "hee";
-}
+struct get_pack_<Variant, 1>
+{
+    get_pack_(const Variant& t) : ob{t} {}
+    auto operator()() {
+        return ob.get_pack();
+    }
+    Variant ob;
 
+};
+template <typename Variant>
+struct get_pack_<Variant, 0>
+{
+    get_pack_(const Variant& t) : ob{t} {}
+    Variant operator()() {
+        return ob;
+    }
+    Variant ob;
 
+};
 
 
 
 template <typename Variant, size_t i>
-std::any gett(Variant& t) {
+std::any gett(const Variant& t) {
     
     //return get_pack_<Variant, i>(t).get_val();
-    return get_pack_<Variant, i>(t);
+    return get_pack_<Variant, i>(t).ob.get_val();
 }
 
 
